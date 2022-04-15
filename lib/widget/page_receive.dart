adbfd20ae525bc02b4693ce3ad6db130a5eb7dd8
choe sangcheon <choi30223@gmail.com>
2022-04-15 15:33:07 +0900

    # 전화 수신 화면에서 조도 센서 값을 콜백하여 화면 검게하는 기능 추가.

--- lib/widget/page_receive.dart
+++ lib/widget/page_receive.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:fake_call/model/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:light/light.dart';
 
 // 수신중 화면.
 class ReceivePage extends StatefulWidget {
@@ -13,120 +16,152 @@ class ReceivePage extends StatefulWidget {
 }
 
 class _ReceivePageState extends State<ReceivePage> {
+  //
+  final Light _lightSensor = Light();
+
+  //
+  late StreamSubscription _subscription;
+
+  bool _isDark = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _subscription.cancel();
+  }
+
   @override
   void initState() {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual);
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    try {
+      _subscription = _lightSensor.lightSensorStream.listen((int luxValue) {
+        print("Lux value: $luxValue");
+        setState(() {
+          _isDark = (luxValue > 10) ? false : true;
+        });
+      });
+    } on LightException catch (exception) {
+      print(exception);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xff4e9fb6),
-              Color(0xff7accb5),
-            ],
-          ),
-        ),
-        child: Container(
-          width: double.infinity,
-          color: Colors.black.withOpacity(0.5),
-          child: Column(
-            children: [
-              Flexible(
-                flex: 1,
+      backgroundColor: Colors.black,
+      body: (!_isDark)
+          ? Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xff4e9fb6),
+                    Color(0xff7accb5),
+                  ],
+                ),
+              ),
+              child: Container(
+                width: double.infinity,
+                color: Colors.black.withOpacity(0.5),
                 child: Column(
                   children: [
-                    AppBar(
-                      backgroundColor: Colors.transparent,
-                      elevation: .0,
-                      centerTitle: true,
-                      title: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(Icons.call, color: Colors.white, size: 16.0),
-                          SizedBox(width: 4.0),
-                          Text('00:08', style: TextStyle(color: Colors.white, fontSize: 16.0)),
+                    Flexible(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          AppBar(
+                            backgroundColor: Colors.transparent,
+                            elevation: .0,
+                            centerTitle: true,
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(Icons.call, color: Colors.white, size: 16.0),
+                                SizedBox(width: 4.0),
+                                Text('00:08',
+                                    style: TextStyle(color: Colors.white, fontSize: 16.0)),
+                              ],
+                            ),
+                            actions: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+                            ],
+                          ),
+                          const SizedBox(height: 48.0),
+                          Text(
+                            widget.settings.name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 36.0,
+                            ),
+                          ),
+                          const SizedBox(height: 6.0),
+                          Text(
+                            '휴대전화 ${widget.settings.number}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ),
+                          ),
                         ],
                       ),
-                      actions: [
-                        IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
-                      ],
                     ),
-                    const SizedBox(height: 48.0),
-                    Text(
-                      widget.settings.name,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 36.0,
-                      ),
-                    ),
-                    const SizedBox(height: 6.0),
-                    Text(
-                      '휴대전화 ${widget.settings.number}',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 32.0),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(32.0),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _buildIconButton(Icons.voicemail_outlined, '녹음'),
+                                _buildIconButton(Icons.videocam_off_outlined, '영상통화'),
+                                _buildIconButton(Icons.bluetooth, '블루투스'),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _buildIconButton(Icons.volume_up, '스피커'),
+                                _buildIconButton(Icons.mic_off_outlined, '내 소리 차단'),
+                                _buildIconButton(Icons.dialpad, '키패드'),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 72.0,
+                              height: 72.0,
+                              child: FloatingActionButton(
+                                onPressed: () => SystemNavigator.pop(),
+                                backgroundColor: Colors.red,
+                                child: const Icon(
+                                  Icons.call_end,
+                                  color: Colors.white,
+                                  size: 32.0,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  margin: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 32.0),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(32.0),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          _buildIconButton(Icons.voicemail_outlined, '녹음'),
-                          _buildIconButton(Icons.videocam_off_outlined, '영상통화'),
-                          _buildIconButton(Icons.bluetooth, '블루투스'),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          _buildIconButton(Icons.volume_up, '스피커'),
-                          _buildIconButton(Icons.mic_off_outlined, '내 소리 차단'),
-                          _buildIconButton(Icons.dialpad, '키패드'),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 72.0,
-                        height: 72.0,
-                        child: FloatingActionButton(
-                          onPressed: () => SystemNavigator.pop(),
-                          backgroundColor: Colors.red,
-                          child: const Icon(
-                            Icons.call_end,
-                            color: Colors.white,
-                            size: 32.0,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+            )
+          : Container(),
     );
   }
 
